7c67fdc0d021c8e7e743349d5ac7958721e120eb
surreale <marcelmakra@gmail.com>
2025-02-26 09:41:58 +0100

    Update_02_26_Button

--- bejelent/bejelent/obj/Debug/net8.0-windows/bejelent.AssemblyInfo.cs
+++ bejelent/bejelent/obj/Debug/net8.0-windows/bejelent.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bejelent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b365737d6c795bd87e9dec22fdbe40c4f7d2cdfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4f6fad0557d8745daf81b602ebb8153f01617a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("bejelent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bejelent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
